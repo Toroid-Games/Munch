3d26d57ab79fbac58f68302e5689864748fb0707
vandeurenglenn <vandeurenglenn@gmail.com>
2022-12-13 20:50:30 +0100

    test: let const

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from './tape.js'
 import { Munch, UnMunch, P } from "./libs.js";
 
-let input = ['what am I supposed to think about', 'One two, buckle my shoe, Ok stop that, have some self respect.',
+const input = ['what am I supposed to think about', 'One two, buckle my shoe, Ok stop that, have some self respect.',
             'Capitals and Punctuation! are not processed', "new lines will also be exposed but this isn't an\nencryption!",
             'unusual characters like ¬ and € or even £ will cause the compression to become larger than the original, So KEEP THESE TO A MINIMUM!',
             'nubers 4 3 2 234', 'is about the speech disorder for goals of treatment for cluttering include unauthorized use is prohibited please be additional strategies that may help people'];
